fdbe4fb2c6fc7361753e66c47c7c5f786973a6e2
Ana Zitzow <anasempai89@gmail.com>
2022-08-12 15:48:28 -0700

    made some changes in the backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
   before_action :authorized_user
 
